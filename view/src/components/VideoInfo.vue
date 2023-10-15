c5e64defd8f006dc2efb7e724ee4844f87c2c569
WeslleyIvis <weslley_reive@hotmail.com>
2023-10-14 23:44:02 -0300

    Video component

---
+++ view/src/components/VideoInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <iframe width="560" height="315" :src="info?.embed?.iframeUrl" :title="info?.title" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        
+        <h2> {{ info?.title }} </h2>
+
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "VideoInfo",
+    
+    props: {
+        info: {
+            type: Object,
+            require: true
+        }
+    }
+
+})
+</script>
